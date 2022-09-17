Fix bug and implement distance computation

src/epic/epic.py
@@ -44,16 +44,19 @@ class EPIC:
         def canonical_reward_fn(state, action, next_state, /):
             state_sample = self.state_sampler.sample()
             action_sample = self.action_sampler.sample()
+            next_state_sample = self.state_sampler.sample()
             # ``state``, ``action``, ``next_state`` correspond to s, a, s' in the paper
-            # and ``state_sample``, ``action_sample`` correspond to S, A, S' in the paper
-            x_kw = utils.keywordize_rew_fn_call(x)
-            x_cart = utils.cartesian_call_wrapper(x_kw)
+            # and ``state_sample``, ``action_sample``, ``next_state_sample``
+            # correspond to S, A, S' in the paper.
+            x_kw = utils.keywordize_rew_fn_call(x)  # call x using keyword arguments
+            x_cart = utils.cartesian_call_wrapper(x_kw)  # automatic cartesian product
+
             # E[R(s', A, S')]. We sample action and next state,
             # and pass in ``next_state`` as the state.
             # We make this the first dimension to then take the mean.
             term_1 = np.mean(
                 x_cart(
-                    {"action": action_sample, "next_state": state_sample},
+                    {"action": action_sample, "next_state": next_state_sample},
                     {"state": next_state},
                 ),
                 axis=0,
@@ -62,14 +65,15 @@ class EPIC:
             # Now it's simply ``state`` that we pass in as the state.
             term_2 = np.mean(
                 x_cart(
-                    {"action": action_sample, "next_state": state_sample},
+                    {"action": action_sample, "next_state": next_state_sample},
                     {"state": state},
                 ),
                 axis=0,
             )
             # E[R(S, A, S')]. We sample state, action, and next state.
-            # This does not require cartesian product over batches as it's not a random variable.
-            term_3 = np.mean(x(state_sample, action_sample, state_sample), axis=0)
+            # This does not require cartesian product over batches
+            # as it's not a random variable.
+            term_3 = np.mean(x(state_sample, action_sample, next_state_sample), axis=0)
 
             return (
                 x(state, action, next_state)
@@ -83,7 +87,14 @@ class EPIC:
     def distance(
         self, x_canonical: types.RewardFunction, y_canonical: types.RewardFunction, /
     ) -> float:
-        raise NotImplementedError
+        state_sample = self.state_sampler.sample()
+        action_sample = self.action_sampler.sample()
+        next_state_sample = self.state_sampler.sample()
+
+        x_samples = x_canonical(state_sample, action_sample, next_state_sample)
+        y_samples = y_canonical(state_sample, action_sample, next_state_sample)
+
+        return np.sqrt(1 - np.corrcoef(x_samples, y_samples))
 
     def epic(self, x: types.RewardFunction, y: types.RewardFunction, /) -> float:
         x_canonical = self.canonicalize(x)
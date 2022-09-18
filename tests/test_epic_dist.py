f2b2c00e112a8e14e8ff75150505b2ccbaf38c3b
Juan Rocamonde <juancarlosrocamonde@gmail.com>
2022-09-18 14:52:50 +0100

    Add tests

---
+++ tests/test_epic_dist.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pytest
+from epic import epic
+import gym
+
+
+def rew_fn_1(state, action, next_state):
+    return state + next_state + action
+
+
+def rew_fn_2(state, action, next_state):
+    return state**2 + next_state**2 + 2 * action
+
+
+def rew_fn_1_potential_shaping(state, action, next_state):
+    return state + next_state + action + next_state / 3 - state / 3
+
+
+def test_epic_dist_no_errors():
+    state_space = gym.spaces.Discrete(10)
+    action_space = gym.spaces.Discrete(10)
+
+    x = rew_fn_1
+    y = rew_fn_2
+
+    dist = epic.EPIC(
+        state_sampler=epic.GymSampler(space=state_space, n_samples=100),
+        action_sampler=epic.GymSampler(space=action_space, n_samples=100),
+        discount_factor=1,
+    ).distance(x, y)
+
+    assert isinstance(dist, float)
+
+
+def test_epic_dist_reward_equivalence():
+    state_space = gym.spaces.Discrete(10)
+    action_space = gym.spaces.Discrete(10)
+
+    x = rew_fn_1
+    y = rew_fn_1_potential_shaping
+
+    dist = epic.EPIC(
+        state_sampler=epic.GymSampler(space=state_space, n_samples=100),
+        action_sampler=epic.GymSampler(space=action_space, n_samples=100),
+        discount_factor=1,
+    ).distance(x, y)
+
+    assert np.isclose(dist, 0, atol=1e-7)
